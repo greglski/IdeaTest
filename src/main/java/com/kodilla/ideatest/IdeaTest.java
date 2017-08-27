b9e854746e15df95ce53a36f27bbf59b864d24b3
Grzegorz Leczkowski <grzegorz.leczkowski@gmail.com>
2017-08-27 17:49:04 +0200

    First commit

---
+++ src/main/java/com/kodilla/ideatest/IdeaTest.java
@@ -0,0 +1,11 @@
+
+package com.kodilla.ideatest;
+public class IdeaTest {
+    public IdeaTest(String print) {
+        System.out.println(print);
+    }
+    public static void main(String[] args) {
+        IdeaTest ideaTest = new IdeaTest("print");
+    }
+}
+
---
+++ src/main/java/com/kodilla/ideatest/IdeaTestApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.ideatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaTestApplication.class, args);
+
+	}
+}
